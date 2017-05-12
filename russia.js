14b8803f92440c412ac4d5d11f085b09c393181c
Butter-Ball <schumach777@gmail.com>
2017-05-12 16:15:07 -0500

    new site

---
+++ russia.js
@@ -0,0 +1,22 @@
+
+//Hide all the stuff I am going to show when button is clicked
+
+$(".container2").hide();
+
+$(".container").click(function(){
+	
+	$(".container").hide();
+	$(".huge-wrapper").addClass("give-background");
+	
+	
+	$(".container2").css("background-color", "#BC0000");
+	$(".container2").css("border", "0px");
+	$(".container2").css("color", "white");
+	
+	$(".container2").show();
+	
+	$("body").append("<audio autoplay src='unionMusic.mp3'>");
+	
+	
+	
+});
